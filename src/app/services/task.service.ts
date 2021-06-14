6d56edfec0cc6a8becf4b092540650f8f0c8d455
ssmith777 <shawn.t.smith.777@gmail.com>
2021-06-14 13:36:29 -0500

    :art: Add observable to service & tasks component

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Task } from '../Task';
 import { TASKS } from '../mock-tasks';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -8,7 +9,8 @@ import { TASKS } from '../mock-tasks';
 export class TaskService {
   constructor() {}
 
-  getTasks(): Task[] {
-    return TASKS;
+  getTasks(): Observable<Task[]> {
+    const tasks = of(TASKS);
+    return tasks;
   }
 }
