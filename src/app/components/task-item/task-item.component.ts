7233ae418f620b68ede0ca9633bb348473bde2f6
ssmith777 <shawn.t.smith.777@gmail.com>
2021-06-14 11:50:38 -0500

    :art: Added service for tasks

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -1,6 +1,6 @@
 import { Task } from './../../Task';
 import { Component, OnInit, Input } from '@angular/core';
-
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-task-item',
   templateUrl: './task-item.component.html',
@@ -9,7 +9,7 @@ import { Component, OnInit, Input } from '@angular/core';
 export class TaskItemComponent implements OnInit {
   @Input()
   task!: Task;
-
+  faTimes = faTimes;
   constructor() {}
 
   ngOnInit(): void {}
